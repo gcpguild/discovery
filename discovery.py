7785dc074bbee61d4b105ffa49df7da0c6bd5bdb
gcpguild <107158741+gcpguild@users.noreply.github.com>
2022-07-30 22:42:54 +0530

    Update discovery.py

--- discovery.py
+++ discovery.py
@@ -394,12 +394,11 @@ for interface_name, interface_addresses in if_addrs.items():
             fv.append(address.address)
             i2 = ("{}{}{}".format("Netmask:", {address.netmask}, ' | '))
             ik2 = ("{}{}{}{}".format('Netmask_', j, '_', k))
-            if (address.netmask):
-                fn.append(ik2)
-                fv.append(address.netmask)
+            fn.append(ik2)
+            fv.append(address.netmask)
             i3 = ("{}{}{}".format("Broadcast IP:", {address.broadcast}, ' | '))
             ik3 = ("{}{}".format('Broadcast_IP_', k))
-            fn.append(ik2)
+            fn.append(ik3)
             fv.append(address.broadcast)
             k += 1
         elif str(address.family) == 'AddressFamily.AF_PACKET':
